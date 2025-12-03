f8966cdd06565903b73a6639ce2019a0fa376997
Mefist89 <jeniabortnic@gmail.com>
2025-12-04 00:28:09 +0200

    fix: bug

--- src/AllExercises.tsx
+++ src/AllExercises.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import TypeExercises from './components/TypeExercises';
 import ExpressionExercises from './components/ExpressionExercises';
 import TruthTableExercises from './components/TruthTableExercises';
